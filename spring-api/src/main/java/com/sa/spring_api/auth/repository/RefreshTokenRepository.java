c3bc2581f1497e08ed39b08a332aa58018ef5c3a
Nirrax <nirraxx@gmail.com>
2026-03-14 02:07:39 +0100

    feat(jwt): add refreshToken repository

---
+++ spring-api/src/main/java/com/sa/spring_api/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,23 @@
+package com.sa.spring_api.auth.repository;
+
+import com.sa.spring_api.auth.entity.RefreshToken;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    @Transactional
+    int deleteByExpiresAtBefore(Instant expiryTime);
+
+    @Modifying
+    void deleteRefreshTokenByUserId(UUID userId);
+}
