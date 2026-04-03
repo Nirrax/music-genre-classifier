54e4679ac56fdf59eaccc02a1a2c27b2146e0c49
Nirrax <nirraxx@gmail.com>
2026-04-03 04:00:49 +0200

    feat(frontend): add component for users homepage

---
+++ react-frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Classifications from "./Classifications";
+import Profile from "./Profile";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Profile />
+      <Classifications />
+    </div>
+  );
+};
+
+export default Home;
